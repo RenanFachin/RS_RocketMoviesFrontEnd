49a58a2eca2c72e0b638a8227de6620d3d4556d2
Renan <renan.fachin@hotmail.com>
2022-08-14 15:33:45 -0300

    Criação do componente Button

--- src/Pages/Details/index.jsx
+++ src/Pages/Details/index.jsx
@@ -1,12 +1,15 @@
 import { Container } from './styles'
 
+import { Button } from '../../components/Button' // importando o componente de button
+
 export function Details (){
 
     return(
         <Container>
         <h1>Hello World!!</h1>
         <span>RocketMovie</span>
+
+        <Button />
         </Container>
     )
-
 }
--- src/styles/theme.js
+++ src/styles/theme.js
@@ -1,20 +1,21 @@
 export default {
     COLORS: {
       // Cores de fundo
-      BACKGROUND_COLOR_900 : "#1C1B1E",
+      BACKGROUND_COLOR_900: "#1C1B1E",
       BACKGROUND_COLOR_800: "#3E3B47",
       BACKGROUND_COLOR_700: "#262529",
       BACKGROUND_COLOR_TAGS: "#312E38",
+      PINK: "#FF859B",
 
       // Cor do SVG
       COR_DO_SVG: "#000000",
 
       // Cores dos textos
       MAIN_COLOR_TEXT_PINK: "#FF859B",
-      COLOR_TEXT_INPUT: "#948F99",
       WHITE: "#FFFFFF",
-      COLOR_TEXT_TAGS: "#E5E5E5",
-      COLOR_TEXT_NOTES: "#F4EDE8",
-  
+      TEXT_INPUT: "#948F99",
+      TEXT_TAGS: "#E5E5E5",
+      TEXT_NOTES: "#F4EDE8",
+      TEXT_BUTTON: "#312E38",
     }
   }
