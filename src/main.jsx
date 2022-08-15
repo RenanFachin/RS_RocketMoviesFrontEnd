53f5f75dba3c061cf32303f64fe7e7012fe9ba71
Renan <renan.fachin@hotmail.com>
2022-08-15 18:43:36 -0300

    Criada as rotas de aplicação da página

--- src/main.jsx
+++ src/main.jsx
@@ -5,13 +5,13 @@ import { ThemeProvider } from 'styled-components' // themeprovider é uma depend
 
 import theme from './styles/theme'// Importação dos temas que serão aplicados à página
 import GlobalStyle from './styles/global' // Importação do arquivo de definições globais da página
-import { New } from './Pages/New' // Página que será renderizada
+import { Routes } from './routes' // Página que será renderizada
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
         <GlobalStyle />
-        <New />
+        <Routes />
     </ThemeProvider>
   </React.StrictMode>
 )
